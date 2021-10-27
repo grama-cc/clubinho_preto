6b57346439ab25dc79ddbb9240d40373ecae13e7
vitusalis <victorsalles1997@gmail.com>
2021-10-27 11:15:28 -0300

    FinanceService get_asaas_subscriptions

--- finance/service.py
+++ finance/service.py
@@ -2,6 +2,7 @@ import requests
 from clubinho_preto.settings import ASAAS_KEY, ASAAS_URL
 from account.service import AccountService
 
+
 class FinanceService:
     @staticmethod
     def asaas_resquest(api_endpoint):
@@ -15,16 +16,32 @@ class FinanceService:
         limit = 100
         customers = FinanceService.asaas_resquest(f'customers?offset={offset}&limit={limit}')
         total_count = customers["totalCount"]
-        while offset < total_count: 
+        while offset < total_count:
             for item in customers["data"]:
                 print(AccountService.update_asaas_customer_id(item["email"], item["id"]))
             offset += limit
             customers = FinanceService.asaas_resquest(f'customers?offset={offset}&limit={limit}')
-
+        return customers
 
     @staticmethod
-    def get_asaas_subscriptions():
-        ids = AccountService.list_asaas_customer_id()
-        for id in ids:
-            subscriptions = FinanceService.asaas_resquest(f'subscriptions?customer={id}')
-        return
+    def get_asaas_subscriptions(customer_id=None):
+        offset = 0
+        limit = 100
+        
+        url = f'subscriptions?offset={offset}&limit={limit}'
+        if customer_id:
+            url += f'&customer={customer_id}'
+                
+        content = FinanceService.asaas_resquest(url)
+        subscriptions = content.get('data')
+        has_more = content.get('hasMore')
+
+        while has_more:
+            offset += limit
+            url = f'subscriptions?offset={offset}&limit={limit}'
+            content = FinanceService.asaas_resquest(url)
+            subscriptions += content.get('data')
+            has_more = content.get('hasMore')
+
+        return subscriptions
+
