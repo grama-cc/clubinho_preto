4a45c697a4cf4bcc468b12958ca081fc66640b02
vitusalis <victorsalles1997@gmail.com>
2021-10-27 17:05:00 -0300

    Import subscriptions as task

--- celery_app/celery.py
+++ celery_app/celery.py
@@ -13,11 +13,23 @@ app.conf.broker_transport_options = {'visibility_timeout': 3600}
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
+
     sender.add_periodic_task(
         crontab(minute=0, hour='*/12'),
+        task_import_subscriptions.s(),
+    )
+
+    sender.add_periodic_task(
+        crontab(minute=30, hour='*/12'),
         task_update_subscriptions.s(),
     )
 
+
+@app.task
+def task_import_subscriptions():
+    from finance.service import FinanceService
+    created, errors = FinanceService.import_asaas_subscriptions()
+    return f'{created} assinaturas criadas, {errors} erros'
 
 @app.task
 def task_update_subscriptions():
