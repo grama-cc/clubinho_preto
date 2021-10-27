70a89c274be97707d2bb96e54c4ce854fed8fca6
vitusalis <victorsalles1997@gmail.com>
2021-10-27 15:34:08 -0300

    Create asaas customer

--- account/service.py
+++ account/service.py
@@ -1,4 +1,10 @@
+from os import name
+import requests
+from clubinho_preto.settings import ASAAS_KEY, ASAAS_URL
+
 from .models import Subscriber
+
+
 class AccountService:
     @staticmethod
     def update_asaas_customer_id(email, id):
@@ -7,4 +13,39 @@ class AccountService:
     @staticmethod
     def list_asaas_customer_id():
         return Subscriber.objects.filter(asaas_customer_id__isnull=False).values_list('asaas_customer_id', flat=True)
-        
+
+    @staticmethod
+    def create_asaas_customer(customer_data):
+
+        # separate asaas data from our data
+        subscriber_keys = ['relatedness', 'more_info', 'relatedness', 'relatedness_raw', 'kids_name',
+                           'kids_age', 'kids_gender', 'kids_race', 'kids_gender_raw', 'kids_race_raw', 'subscribing_date', ]
+
+        subscriber_data = {key: customer_data[key] for key in customer_data if key in subscriber_keys}
+
+        for key, value in subscriber_data.items():
+            customer_data.pop(key)
+
+        url = f"{ASAAS_URL}/customers"
+        response = requests.post(url, json=customer_data, headers={'access_token': ASAAS_KEY})
+        if response.ok:
+            data = response.json()
+
+            try:
+                Subscriber.objects.create(
+                    asaas_customer_id=data.get('id', None),
+                    cep=data.get('postalCode', None),
+
+                    name=data.get('name', None),
+                    email=data.get('email', None),
+                    phone=data.get('phone', None),
+                    address=data.get('address', None),
+
+                    **subscriber_data
+                )
+            except Exception as e:
+                print(f'______COULD NOT CREATE SUBSCRIBER: {e}')
+                # todo: handle error: delete customer from asaas?
+                # todo: create asaas customer on Subscriber model's save()?
+
+        return response
---
+++ finance/management/commands/create_fake_subscriber.py
@@ -0,0 +1,47 @@
+import json
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Test command'
+
+    def handle(self, *args, **options):
+        from account.service import AccountService
+        data = {
+            "name": "Marcelo Almeida",
+            "email": "marcelo.almeida@gmail.com",
+            "phone": "4738010919",
+            "mobilePhone": "4799376637",
+            "cpfCnpj": "24971563792",
+            "postalCode": "01310-000",
+            "address": "Av. Paulista",
+            "addressNumber": "150",
+            "complement": "Sala 201",
+            "province": "Centro",
+            "externalReference": "12987382",
+            "notificationDisabled": False,
+            "additionalEmails": "marcelo.almeida2@gmail.com,marcelo.almeida3@gmail.com",
+            "municipalInscription": "46683695908",
+            "stateInscription": "646681195275",
+            "observations": "ótimo pagador, nenhum problema até o momento",
+
+            "relatedness": "PA",
+            "relatedness_raw": "pai",
+            "kids_name": "Criança filha",
+            "kids_age": "8",
+            "kids_gender": "MA",
+            "kids_race": "YE",
+            "kids_gender_raw": "menino",
+            "kids_race_raw": "asiática",
+            "subscribing_date": "2020-02-02",
+        }
+        response = AccountService.create_asaas_customer(data)
+
+        if response.ok:
+            print("Criado com sucesso")
+        else:
+            if response.content:
+                content = json.loads(response.content)
+                print(content)
+            else:
+                print('Não há mensagem de erro')
